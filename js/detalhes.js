ff518052c5f045d8e082bb577f1c74f08c87ff7e
Gabriel Miranda <51680857+gabrieljjm@users.noreply.github.com>
2020-01-10 21:03:44 +0000

    Primeiro

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,12 +1,17 @@
-function show(data){
-    return  "<h4><strong>Cidade/País</strong>: "+ data.name  +"</h4>" +
-        "<h4><strong>Tempo</strong>: "+ data.weather[0].main +"</h4>" +
-        "<h4><strong>Descrição</strong>: "+ data.weather[0].description +"</h4>" +
-        "<h4><strong>Temperatura (celsius) </strong>: "+ data.main.temp +"º "+"</h4>" +
-        "<h4><strong>Humidade</strong>: "+ data.main.humidity +"% "+"</h4>" +
-        "<h4><strong>Temperatura mínima</strong>: "+ data.main.temp_min +"º "+"</h4>" +
-        "<h4><strong>Temperatura máxima</strong>: "+ data.main.temp_max +"º "+"</h4>"
-}
+$(document).ready(function () {
+    var parametros = recebeURL();
+    var id = parametros.id;
+    $.ajax({
+        url:'http://api.openweathermap.org/data/2.5/weather?id=' + id + "&units=metric&lang=pt" + "&APPID=0799ebbcad862deddeeebe1f4ccc65d7",
+        type:"GET",
+        dataType:"json",
+        success: function(data){
+            var widget = show(data);
+            $("#show").html (widget);
+        }
+
+    });
+})
 
 // Botão Pesquisar
 function btnPesquisar() {
@@ -16,6 +21,18 @@ function btnPesquisar() {
     }else {
         document.getElementById("city").style.color = "red";
     }
+}
+
+function show(data){
+    return  "<h3><strong>Cidade/País</strong>: "+ data.name  +"</h3>" +
+        "<h3><strong>Tempo</strong>: "+ data.weather[0].main +"</h3>" +
+        "<h3><strong>Descrição</strong>: "+ data.weather[0].description +"</h3>" +
+        "<h3><strong>Temperatura (celsius) </strong>: "+ data.main.temp +"º "+"</h3>" +
+        "<h3><strong>Humidade</strong>: "+ data.main.humidity +"% "+"</h3>" +
+        "<h3><strong>Temperatura mínima</strong>: "+ data.main.temp_min +" º"+"</h3>" +
+        "<h3><strong>Temperatura máxima</strong>: "+ data.main.temp_max +" º"+"</h3>"
+
+
 }
 
 // Verifica Campo da Cidade/Pais
@@ -32,7 +49,7 @@ function recebeOpenWeather(cidade) {
     $.ajax({
         url:'http://api.openweathermap.org/data/2.5/weather?q=' + cidade + "&units=metric&lang=pt" + "&APPID=0799ebbcad862deddeeebe1f4ccc65d7",
         type:"GET",
-        dataType:"jsonp",
+        dataType:"json",
         success: function(data){
             var widget = show(data);
             $("#show").html (widget);
@@ -41,4 +58,13 @@ function recebeOpenWeather(cidade) {
     });
 }
 
-
+function recebeURL() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
--- js/forecast.js
+++ js/forecast.js
@@ -1,11 +1,28 @@
+var id;
+var mdtemp = [];
+var tempmax = [];
+var tempmin = [];
+var desc = [];
+
+function atualizaTabelas(dados) {
+    fazMedia(dados);
+    var i = 0;
+    while (0 < 5){
+        document.getElementById(('dia'+ i)).innerHTML = dados.name + ", " + dados.sys.country;
+        i++;
+    }
+}
+
 function show(data){
     return  "<h3><strong>Cidade/País</strong>: "+ data.city.name+ ", "  + data.city.country +" </h3>"
                 +"<br>"+
             "<h4><strong>Temperatura minima</strong>: "+  data.list[0].main.temp_min +" </h4>"+
              "<h4><strong>Temperatura máxima</strong>: "+  data.list[0].main.temp_max +" </h4>"+
-             "<h4><strong>Data/hora</strong>: "+  data.list[0].dt_txt +" </h4>"
-
-
+             "<h4><strong>Data/hora</strong>: "+  data.list[0].dt_txt +" </h4>"+
+            "<br>"+
+            "<h4><strong>Temperatura minima</strong>: "+  data.list[1].main.temp_min +" </h4>"+
+            "<h4><strong>Temperatura máxima</strong>: "+  data.list[1].main.temp_max +" </h4>"+
+            "<h4><strong>Data/hora</strong>: "+  data.list[1].dt_txt +" </h4>"
 }
 
 // Botão Pesquisar
@@ -13,8 +30,6 @@ function btnPesquisar() {
     cidade = document.getElementById("city").value;
     if (verificaCampoPreenchido(cidade)){
         recebeOpenWeather(cidade)
-    }else {
-        document.getElementById("city").style.color = "red";
     }
 }
 
@@ -32,12 +47,87 @@ function recebeOpenWeather(cidade) {
     $.ajax({
         url:'http://api.openweathermap.org/data/2.5/forecast?q=' + cidade + "&units=metric&lang=pt" + "&APPID=0799ebbcad862deddeeebe1f4ccc65d7",
         type:"GET",
-        dataType:"jsonp",
-        success: function(data){
-            console.log(data);
-            var widget = show(data);
-            $("#show").html (widget);
+        dataType:"json",
+        success: function(dadosOpenWeather){
+            //existeLocalStorage
+            console.log(dadosOpenWeather);
+            atualizaTabelas(dadosOpenWeather);
+            //var media = getMedia(dadosOpenWeather);
+            //atualizaTabela1(dadosOpenWeather);
         }
-
     });
 }
+
+function fazMedia(dados) {
+    var i = 0;
+    var j = 0;
+    var c = 0;
+    var divide = dados.list[0].dt_txt.split(" ");
+    var dt1 = divide[0];
+    while (i < 5){
+        console.log(mdtemp[i]);
+        mdtemp[i] = 0;
+        tempmax[i] = 0;
+        tempmin[i] = 100;
+        c = 0;
+        while (dt1 === (dados.list[j].dt_txt.split(" "))[0]){
+            mdtemp[i] += dados.list[j].main.temp;
+            if (tempmax[i] < dados.list[j].main.temp_max){
+                tempmax[i] = dados.list[j].main.temp_max;
+            }
+            if (tempmin[i] > dados.list[j].main.temp_min){
+                tempmin[i] = dados.list[j].main.temp_min;
+            }
+            desc[i] = dados.list[j].weather[0].description;
+            console.log(dados.list[j].weather[0].description);
+            c += 1;
+            console.log(dados.list[j].main.temp + "  " + j + "  " + (dados.list[j].dt_txt.split(" "))[1]);
+            j++;
+        }
+        mdtemp[i] /= c;
+        divide = dados.list[j].dt_txt.split(" ");
+        dt1 = divide[0];
+        i++;
+        console.log(i);
+        console.log(mdtemp+tempmax+tempmin);
+    }
+    console.log(mdtemp);
+    console.log(tempmax);
+    console.log(tempmin);
+    console.log(desc);
+}
+
+
+
+
+/*
+function fazMedia(dados) {
+    var i = 0;
+    var j = 0;
+    var media = {};
+    var divide = dados.list[0].dt_txt.split(" ");
+    var dt1 = divide[0];
+    while (i < 5){
+        console.log(media[i]);
+        media[i] = {"campo1": 0};
+        media[i] = {"campo2": 0};
+        while (dt1 === (dados.list[j].dt_txt.split(" "))[0]){
+            media[i].campo1 += dados.list[j].main.temp;
+            media[i].campo2 += 1;
+            console.log(dados.list[j].main.temp + "  " + j + "  " + (dados.list[j].dt_txt.split(" "))[1]);
+            j++;
+        }
+        media[i].campo1 /= media[i].campo2;
+        divide = dados.list[j].dt_txt.split(" ");
+        dt1 = divide[0];
+        i++;
+        console.log(i);
+        console.log(media);
+    }
+    console.log(media);
+}
+
+
+function atualizaTabela1(dados) {
+    while ()
+}*/
